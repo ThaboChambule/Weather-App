6674f20f280f47e7a3f9615b66a8552544dd05d7
thabo <thabochambule1@gmail.com>
2024-10-12 05:41:20 +0200

    add functionality to search

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,30 @@
 
-const apiUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Bloemfontein?unitGroup=metric&key=5BSKWDZYG3QG7X5CSQZSZFVE4&contentType=json"
+const search = document.querySelector(".search-icon").addEventListener("click", ()=>{
+const input = document.querySelector(".input");
+const city = input.value;
+console.log(city);
+
+ const apiUrl =
+  `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=5BSKWDZYG3QG7X5CSQZSZFVE4&contentType=json`;
 
 fetch(apiUrl)
-.then(function(response){
+  .then(function (response) {
     return response.json();
-})
-.then(function(response){
-    console.log(response)
+  })
+  .then(function (response) {
+    console.log(response);
     document.querySelector(".city").innerHTML = response.address;
+    document.querySelector(".temp").innerHTML =
+      response.currentConditions.temp + "°C";
+    document.querySelector(".humidity").innerHTML =
+      response.currentConditions.humidity + "%";
+    document.querySelector(".wind").innerHTML =
+      response.currentConditions.windspeed + " km/h";
+  });
 
 
-})
-   
+}
+
+)
+
+
